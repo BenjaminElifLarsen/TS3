cbcdd396bfece7e393f76c004991a50f18f22882
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-12-07 15:29:21 +0100

    working version

--- Program.cs
+++ Program.cs
@@ -12,19 +12,20 @@ namespace TS3
         static string[] information = new string[4];
         static void Main(string[] args)
         {
-            if(args.Length == 4)
-            {
-                information[0] = args[0];
-                information[1] = args[1];
-                information[2] = args[2];
-                information[3] = args[3];
-            }
-            else
+            if(args.Length >= 0 && args.Length < 4)
             {
                 information[0] = "6R6Z-74TE-59CF-MRJ5-2RO2-HOD9";
                 information[1] = "10.11.5.197";
                 information[2] = "+cFwYoa5";
                 information[3] = "ClientQuery";
+                Console.WriteLine("TS3.exe key server password userName");
+            }
+            else if(args.Length == 4)
+            {
+                information[0] = args[0]; //key
+                information[1] = args[1]; //server
+                information[2] = args[2]; //password
+                information[3] = args[3]; //user name
             }
             Connent();
             
@@ -64,24 +65,35 @@ namespace TS3
              * When a port transmit a response back it will be arudiono. Keep the port open and enter the while loop
             */
             SerialPort serialPort = new SerialPort();
+            int position = 0;
             for (int i = 0; i < test.Length; i++)
             {
                 serialPort = new SerialPort(test[i], 9600);
                 serialPort.Open();
                 serialPort.Write("1");
                 Thread.Sleep(1500);
-                byte[] dataArduino = new byte[0];
-                serialPort.Read(dataArduino, 0, 1);
-                if (dataArduino[0] == 1)
-                    break;
+                byte[] dataArduino = new byte[1];
+                serialPort.ReadTimeout = 1500;
+                try
+                {
+                    serialPort.Read(dataArduino, 0, 1);
+                    position = i;
+                    if (dataArduino[0] == 1)
+                        break;
+                }
+                catch
+                {
+
+                }
                 serialPort.Close();
             }
-
+            serialPort = new SerialPort(test[position], 9600);
+            serialPort.Open();
 
             Console.WriteLine("Loop");
             while (true) //move this into a function
             {
-                Console.WriteLine(Environment.NewLine + "Client List"); //will return a string that can be used to find the different client IDs
+                //Console.WriteLine(Environment.NewLine + "Client List"); //will return a string that can be used to find the different client IDs
                 message = "clientlist -voice\n"; //"clientlist \n"
                 data = Encoding.ASCII.GetBytes(message);
                 networkStream.Write(data, 0, data.Length);
