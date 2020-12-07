f27257456873234207b66eb863d5d817391a5787
Benjamin Elif Larsen <benj158d@elev.techcollege.dk>
2020-12-07 15:13:46 +0100

    newest

--- Program.cs
+++ Program.cs
@@ -9,18 +9,35 @@ namespace TS3
 {
     class Program
     {
+        static string[] information = new string[4];
         static void Main(string[] args)
         {
-            Test();
+            if(args.Length == 4)
+            {
+                information[0] = args[0];
+                information[1] = args[1];
+                information[2] = args[2];
+                information[3] = args[3];
+            }
+            else
+            {
+                information[0] = "6R6Z-74TE-59CF-MRJ5-2RO2-HOD9";
+                information[1] = "10.11.5.197";
+                information[2] = "+cFwYoa5";
+                information[3] = "ClientQuery";
+            }
+            Connent();
+            
         }
 
-        static void Test()
+        static void Connent()
         {
             TcpClient tcpClient = new TcpClient("127.0.0.1", 25639);
 
             Console.WriteLine("Auth");
             NetworkStream networkStream = tcpClient.GetStream();
-            string message = "auth apikey=6R6Z-74TE-59CF-MRJ5-2RO2-HOD9\n"; //Those \n are needed from what I have read
+            string message = $"auth apikey={information[0]}\n"; //argument 0
+            //Those \n are needed from what I have read
             byte[] data = Encoding.ASCII.GetBytes(message);
             networkStream.Write(data, 0, data.Length);
             Thread.Sleep(1000);
@@ -29,18 +46,9 @@ namespace TS3
             Console.WriteLine(Encoding.ASCII.GetString(data, 0, bytes));
             Console.WriteLine();
 
-            //Console.WriteLine("Help");
-            //message = "help clientvariable client_unique_Identifier\n"; //find help on a specific function
-            //data = Encoding.ASCII.GetBytes(message);
-            //networkStream.Write(data, 0, data.Length);
-            //Thread.Sleep(1000); //these delays are present to ensure that there are data to read
-            //data = new byte[2048];
-            //bytes = networkStream.Read(data, 0, data.Length);
-            //Console.WriteLine(Encoding.ASCII.GetString(data, 0, bytes));
-            //Console.WriteLine();
 
             Console.WriteLine("Connection"); //will return a string that can be used to find the different client IDs
-            message = "connect address=10.11.5.197 password=+cFwYoa5 nickname=ClientQuery\n";
+            message = $"connect address={information[1]} password={information[2]} nickname={information[3]}\n"; //argument 1 2 3
             data = Encoding.ASCII.GetBytes(message);
             networkStream.Write(data, 0, data.Length);
             Thread.Sleep(1000);
@@ -49,49 +57,36 @@ namespace TS3
             Console.WriteLine(Encoding.ASCII.GetString(data, 0, bytes));
             Console.WriteLine();
 
-            //Console.WriteLine("Client List"); //will return a string that can be used to find the different client IDs
-            //message = "clientlist\n";
-            //data = Encoding.ASCII.GetBytes(message);
-            //networkStream.Write(data, 0, data.Length);
-            //Thread.Sleep(1000);
-            //data = new byte[256];
-            //bytes = networkStream.Read(data, 0, data.Length);
-            //Console.WriteLine(Encoding.ASCII.GetString(data, 0, bytes));
-            //Console.WriteLine();
-
-            //Console.WriteLine("Unique Identifier"); //get the unique identifier for client 1.
-            //message = "clientvariable clid=4 client_unique_identifier\n";
-            //data = Encoding.ASCII.GetBytes(message);
-            //networkStream.Write(data, 0, data.Length);
-            //Thread.Sleep(1000);
-            //data = new byte[256];
-            //bytes = networkStream.Read(data, 0, data.Length);
-            //Console.WriteLine(Encoding.ASCII.GetString(data, 0, bytes));
-            //Console.WriteLine();
-
-            //Console.WriteLine("Flag"); //get the flag for client 1
-            //message = "clientvariable clid=2 client_flag_talking\n";
-            //data = Encoding.ASCII.GetBytes(message);
-            //networkStream.Write(data, 0, data.Length);
-            //Thread.Sleep(1000);
-            //data = new byte[256];
-            //bytes = networkStream.Read(data, 0, data.Length);
-            //Console.WriteLine(Encoding.ASCII.GetString(data, 0, bytes));
-
             string[] test = SerialPort.GetPortNames();
-            SerialPort serialPort = new SerialPort(test[0], 9600);
-            serialPort.Open();
+            /*need to get the port the arduino is connected too. Could do something similar to the python program
+             * Loop through each port. For each port, connect and transmit a string. If there is no respone after like 1 second
+             * Close the connection and try the next port.
+             * When a port transmit a response back it will be arudiono. Keep the port open and enter the while loop
+            */
+            SerialPort serialPort = new SerialPort();
+            for (int i = 0; i < test.Length; i++)
+            {
+                serialPort = new SerialPort(test[i], 9600);
+                serialPort.Open();
+                serialPort.Write("1");
+                Thread.Sleep(1500);
+                byte[] dataArduino = new byte[0];
+                serialPort.Read(dataArduino, 0, 1);
+                if (dataArduino[0] == 1)
+                    break;
+                serialPort.Close();
+            }
 
 
             Console.WriteLine("Loop");
-            while (true)
+            while (true) //move this into a function
             {
                 Console.WriteLine(Environment.NewLine + "Client List"); //will return a string that can be used to find the different client IDs
                 message = "clientlist -voice\n"; //"clientlist \n"
                 data = Encoding.ASCII.GetBytes(message);
                 networkStream.Write(data, 0, data.Length);
                 Thread.Sleep(250);
-                data = new byte[2048]; //Need to read while data is availible
+                //Need to read while data is availible
                 List<byte> bytelist = new List<byte>();
                 byte[] smallArray = new byte[1];
                 while (networkStream.DataAvailable)
@@ -99,24 +94,23 @@ namespace TS3
                     networkStream.Read(smallArray, 0, smallArray.Length);
                     bytelist.Add(smallArray[0]);
                 }
-                //bytes = networkStream.Read(data, 0, data.Length);
                 string info = Encoding.ASCII.GetString(bytelist.ToArray(), 0, bytelist.Count);
                 string[] users = info.Split('|');
-
+                int pos = 0;
                 foreach(string user in users) 
                 {
+                    pos++;
                     string userID = user.Split(' ')[0];
-                    Console.Write(userID + " ");
-                    Console.WriteLine(user.Split(' ')[5]);
-                    //message = $"clientvariable {userID} client_flag_talking\n";
-                    //data = Encoding.ASCII.GetBytes(message);
-                    //networkStream.Write(data, 0, data.Length);
-                    //Thread.Sleep(250);
-                    //data = new byte[256];
-                    //bytes = networkStream.Read(data, 0, data.Length);
-                    //string talkingInfo = Encoding.ASCII.GetString(data, 0, bytes);
-
-                    //Console.WriteLine(talkingInfo.Split(' ')[1].Split('\n')[0]);
+                    //Console.Write(userID + " ");
+                    //Console.WriteLine(user.Split(' ')[5]);
+                    if (user.Split(' ')[5].Split('=')[1] == "1") {
+                        serialPort.Write("1");
+                        break;
+                    }
+                    else if(user.Split(' ')[5].Split('=')[1] == "0" && pos == users.Length)
+                    {
+                        serialPort.Write("0");
+                    }
                 }
             }
         }
