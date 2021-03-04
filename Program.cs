b53552e278afa9b7a8fbd5c47998a3d359bd94dc
Benjamin Larsen <beela93@hotmail.com>
2021-03-04 07:50:38 +0100

    added a class, written by someone else, cleaned some codes, and moved code into the more functions

--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
-﻿using System;
+﻿using SerialPinComander.Communication;
+using System;
 using System.Collections.Generic;
 using System.IO.Ports;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace TS3
 {
     class Program
     {
         static string[] information = new string[4];
+
+        private static SerialPinCommander spc = SerialPinCommander.GetCommander();
+        private static TcpClient tcpClient;
         static void Main(string[] args)
         {
             if(args.Length >= 0 && args.Length < 4)
@@ -26,96 +31,82 @@ namespace TS3
                 information[1] = args[1]; //server
                 information[2] = args[2]; //password
                 information[3] = args[3]; //user name
+                spc.ConnectionSuccess += SucceededConnection;
+                spc.ConnectionFail += FailedConnection;
+                ArduinoConnection();
+                NetworkStream nws = TS3Connection();
+                Transmitter(nws);
             }
-            Connent();
             
         }
 
-        static void Connent()
+        /// <summary>
+        /// Will try and connection to a TS3 sever using the information in the information array.
+        /// 
+        /// </summary>
+        static NetworkStream TS3Connection()
         {
-            TcpClient tcpClient = new TcpClient("127.0.0.1", 25639);
-
-            Console.WriteLine("Auth");
-            NetworkStream networkStream = tcpClient.GetStream();
-            string message = $"auth apikey={information[0]}\n"; //argument 0
-            //Those \n are needed from what I have read
-            byte[] data = Encoding.ASCII.GetBytes(message);
-            networkStream.Write(data, 0, data.Length);
-            Thread.Sleep(1000);
-            data = new byte[256];
-            int bytes = networkStream.Read(data, 0, data.Length);
-            Console.WriteLine(Encoding.ASCII.GetString(data, 0, bytes));
-            Console.WriteLine();
-
-
-            Console.WriteLine("Establishing Connection"); //will return a string that can be used to find the different client IDs
-            message = $"connect address={information[1]} " +
-                $"password={information[2]} " +
-                $"nickname={information[3]} " +
-                $"channel={"Vejkanalen"}\n"; //argument 1 2 3
-            data = Encoding.ASCII.GetBytes(message);
-            networkStream.Write(data, 0, data.Length);
-            Thread.Sleep(1000);
-            data = new byte[256];
-            bytes = networkStream.Read(data, 0, data.Length);
-            Console.WriteLine("Received");
-            Console.WriteLine(Encoding.ASCII.GetString(data, 0, bytes));
-            Console.WriteLine("Hvis der er error ids som ikke er lig med 0, betyder det at programmet ikke har lykkes at oprette forbindelse til serveren." +
-                Environment.NewLine + "Tjek om kodeord og brugernavn er korrekt. " +
-                Environment.NewLine + "Tjek også om brugeren allerede har oprettet forbindelse til serveren. Hvis de har, sluk for forbindelsen til serveren og start dette program igen.");
-            
-
-            string[] test = SerialPort.GetPortNames();
-            /*need to get the port the arduino is connected too. Could do something similar to the python program
-             * Loop through each port. For each port, connect and transmit a string. If there is no respone after like 1 second
-             * Close the connection and try the next port.
-             * When a port transmit a response back it will be arudiono. Keep the port open and enter the while loop
-            */
-            SerialPort serialPort = new SerialPort();
-            int position = 0;
-            for (int i = 0; i < test.Length; i++)
+            try
             {
-                serialPort = new SerialPort(test[i], 9600);
-                serialPort.Open();
-                serialPort.Write("1");
-                Thread.Sleep(1500);
-                byte[] dataArduino = new byte[1];
-                serialPort.ReadTimeout = 2500;
-                try
-                {
-                    serialPort.Read(dataArduino, 0, 1);
-                    position = i;
-                    Console.WriteLine($"Prøver at forbinde til: {test[i]}");
-                    if (dataArduino[0] == 1)
-                    {
-                        Console.WriteLine("Arudiono var fundet");
-                        break;
-                    }
-                    else if (i == test.Length - 1)
-                    {
-                        Console.WriteLine("Arduino var ikke fundet. Tryk Enter for at afsluttet programmet.");
-                        Console.ReadLine();
-                        Environment.Exit(1);
-                    }
-                }
-                catch
-                {
+                tcpClient = new TcpClient("127.0.0.1", 25639);
 
-                }
-                serialPort.Close();
+                Console.WriteLine("Auth");
+                NetworkStream networkStream = tcpClient.GetStream();
+                string message = $"auth apikey={information[0]}\n"; //argument 0
+                //Those \n are needed in the strings messages else TS3 will not react correctly on them
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                networkStream.Write(data, 0, data.Length);
+                Thread.Sleep(1000);
+                data = new byte[256];
+                int bytes = networkStream.Read(data, 0, data.Length);
+                Console.WriteLine(Encoding.ASCII.GetString(data, 0, bytes));
+                Console.WriteLine();
+
+                Console.WriteLine("Establishing Connection");
+                message = $"connect address={information[1]} " +
+                    $"password={information[2]} " +
+                    $"nickname={information[3]} " +
+                    $"channel={"Vejkanalen"}\n"; //argument 1 2 3
+                data = Encoding.ASCII.GetBytes(message);
+                networkStream.Write(data, 0, data.Length);
+                Thread.Sleep(1000);
+                data = new byte[256];
+                bytes = networkStream.Read(data, 0, data.Length);
+                Console.WriteLine("Received");
+                Console.WriteLine(Encoding.ASCII.GetString(data, 0, bytes));
+                Console.WriteLine("Hvis der er error ids som ikke er lig med 0, betyder det at programmet ikke har lykkes at oprette forbindelse til serveren." +
+                    Environment.NewLine + "Tjek om kodeord og brugernavn er korrekt. " +
+                    Environment.NewLine + "Tjek også om brugeren allerede har oprettet forbindelse til serveren. Hvis de har, sluk for forbindelsen til serveren og start dette program igen.");
+
+                return networkStream;
             }
-            serialPort = new SerialPort(test[position], 9600);
-            serialPort.Open();
-
-            Console.WriteLine("Loop");
-            while (true) //move this into a function
+            catch (Exception e)
             {
-                //Console.WriteLine(Environment.NewLine + "Client List"); //will return a string that can be used to find the different client IDs
+                Console.WriteLine("Fejlede i at oprette forbindelse til TS3." + Environment.NewLine + "Tjek om TS3 er kørende. Hvis ikke, start programmet, men log ikke på serveren.");
+                Console.WriteLine("Fejlbesked:" + Environment.NewLine + e.Message);
+                return null;
+            }
+        }
+
+        private static void Transmitter(NetworkStream networkStream)
+        {
+            if(networkStream == null)
+            {
+                Console.WriteLine("Netværkstrøm er nul. Tryk Enter for at afsluttet programmet.");
+                Console.ReadLine();
+                Environment.Exit(1);
+            }
+            string message;
+            byte[] data;
+            while (!spc.IsConnected) { }
+            Console.WriteLine("Loop");
+            while (true)
+            {
+                //will return a string that can be used to find the different client IDs
                 message = "clientlist -voice\n"; //"clientlist \n"
                 data = Encoding.ASCII.GetBytes(message);
                 networkStream.Write(data, 0, data.Length);
                 Thread.Sleep(250);
-                //Need to read while data is availible
                 List<byte> bytelist = new List<byte>();
                 byte[] smallArray = new byte[1];
                 while (networkStream.DataAvailable)
@@ -127,22 +118,38 @@ namespace TS3
                 string[] users = info.Split('|');
                 int pos = 0;
                 Console.WriteLine($"Fandt {users.Length} brugere");
-                foreach(string user in users) 
+                foreach (string user in users)
                 {
                     pos++;
                     string userID = user.Split(' ')[0];
-                    //Console.Write(userID + " ");
-                    //Console.WriteLine(user.Split(' ')[5]);
-                    if (user.Split(' ')[5].Split('=')[1] == "1") {
-                        serialPort.Write("1");
+                    if (user.Split(' ')[5].Split('=')[1] == "1")
+                    {
+                        spc.State = SerialPinCommander.SerialTS3State.ResiveData;
                         break;
                     }
-                    else if(user.Split(' ')[5].Split('=')[1] == "0" && pos == users.Length)
+                    else if (user.Split(' ')[5].Split('=')[1] == "0" && pos == users.Length)
                     {
-                        serialPort.Write("0");
+                        spc.State = SerialPinCommander.SerialTS3State.NoResivingData;
                     }
                 }
             }
+        }
+
+        public static async Task ArduinoConnection()
+        {
+            await spc.ConnectAsync();
+        }
+
+        private static void FailedConnection(object sender, EventArgs e)
+        {
+            Console.WriteLine("Arduino blev ikke fundet. Tryk Enter for at afsluttet programmet.");
+            Console.ReadLine();
+            Environment.Exit(1);
+        }
+
+        private static void SucceededConnection(object sender, EventArgs e)
+        {
+            Console.WriteLine("Arudiono blev fundet");
         }
     }
 }
---
+++ SerialPinComander.Communication_SerialPinCommander.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialPinComander.Communication
+{
+    /// <summary>
+    /// A class to commicate to SerialPinCommand on Arduino Leonardo from TeamSpeek3 Program (TS3)
+    /// </summary>
+    public class SerialPinCommander
+    {
+        /// <summary>
+        /// This instance of commander for singleton
+        /// </summary>
+        private static SerialPinCommander serialPinCommander;
+
+        /// <summary>
+        /// PortRange for Arduino Leonardo Serial Speed
+        /// </summary>
+        private const int portRange = 9600;
+
+        /// <summary>
+        /// Last resived data from Arduino
+        /// </summary>
+        private string data = string.Empty;
+
+        /// <summary>
+        /// TS3 States for the Arduino Leonardo 5v output 
+        /// </summary>
+        public enum SerialTS3State
+        {
+            /// <summary>
+            /// Tell the arduino to not seding 5v to radio
+            /// </summary>
+            NoResivingData,
+            /// <summary>
+            /// Tell the arduino to seding 5v to radio
+            /// </summary>
+            ResiveData
+        }
+
+        /// <summary>
+        /// Get or set the current state
+        /// </summary>
+        public SerialTS3State State { get; set; }
+
+        /// <summary>
+        /// Current Serial port the arduino are connected to
+        /// </summary>
+        private SerialPort currentSerialPort;
+
+        /// <summary>
+        /// Get the info abute is Arduino are connected or not
+        /// </summary>
+        public bool IsConnected { private set; get; }
+
+        /// <summary>
+        /// Set this class to only be init by a singleton
+        /// </summary>
+        private SerialPinCommander()
+        {
+
+        }
+
+        /// <summary>
+        /// Get the SerialPincommand with the connection to Arduino
+        /// </summary>
+        /// <returns></returns>
+        public static SerialPinCommander GetCommander()
+        {
+            if (serialPinCommander is null)
+                serialPinCommander = new SerialPinCommander();
+            return serialPinCommander;
+        }
+
+        /// <summary>
+        /// Resive data 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ResiceData_Event(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (sender is SerialPort)
+            {
+                var port = (SerialPort)sender;
+                data = ResiveData(ref port);
+                Console.WriteLine(data);
+            }
+        }
+
+        /// <summary>
+        /// Get data from current Serial commication port
+        /// </summary>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        private string ResiveData(ref SerialPort port)
+        {
+            int bufferSize = port.BytesToRead;
+            byte[] buffer = new byte[bufferSize];
+            port.Read(buffer, 0, bufferSize);
+            return Encoding.Default.GetString(buffer);
+        }
+
+        /// <summary>
+        /// Send data to to current Serial commication port
+        /// </summary>
+        /// <param name="port"></param>
+        /// <param name="data"></param>
+        private void SendData(ref SerialPort port, string data)
+        {
+            byte[] buffer = Encoding.Default.GetBytes(data);
+            port.Write(buffer, 0, buffer.Length);
+        }
+
+        /// <summary>
+        /// Starte to sending a stream of 8-bit's '1's to Arduino Leonardo
+        /// </summary>
+        private async void StartSender()
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    if (State == SerialTS3State.ResiveData)
+                        SendData(ref currentSerialPort, "1");
+                }
+            });
+        }
+
+        /// <summary>
+        /// Connection fail event
+        /// </summary>
+        public event EventHandler ConnectionFail;
+
+        /// <summary>
+        /// Connection success event
+        /// </summary>
+        public event EventHandler ConnectionSuccess;
+
+        /// <summary>
+        /// Connect to Arduino Leonardo Serial Port
+        /// </summary>
+        /// <returns></returns>
+        public async Task ConnectAsync()
+        {
+            //
+            if (!IsConnected)
+            {
+                // Get all com serial names
+                var portNames = SerialPort.GetPortNames().ToList();
+
+                SerialPort serialPort;
+
+                // Loop all com port names 
+                foreach (var name in portNames.OrderByDescending(i => portNames.IndexOf(i)))
+                {
+                    // Target new comport by comport name
+                    serialPort = new SerialPort(name, portRange, Parity.None);
+
+                    try
+                    {
+                        // Set the RTS to enable
+                        serialPort.RtsEnable = true;
+                        // Set the data resive event
+                        serialPort.DataReceived += ResiceData_Event;
+                        // Open the serialport
+                        serialPort.Open();
+                        // Send '1' to arduino to get a response for to check for is the rigth arduino devcice 
+                        SendData(ref serialPort, "1");
+                        // Wait 2sek for response data
+                        await Task.Delay(2000);
+                        // If data has '1' the connection istablish 
+                        if (data.Equals("1"))
+                        {
+                            // Set the connection is connectet to true
+                            IsConnected = true;
+                            // Set the current serial port
+                            currentSerialPort = serialPort;
+                            // Start sender to to send stream of '1' if in the rigth state
+                            StartSender();
+                            // Call connection success event if et connectet
+                            ConnectionSuccess?.Invoke(this, new EventArgs());
+                        }
+                        else
+                        {
+                            // Call connection fail event if et not connectet
+                            ConnectionFail?.Invoke(this, new EventArgs());
+                        }
+                    }
+
+                    finally
+                    {
+                        // If port not are connected to rigth Arduino close serila connection if is has one
+                        if (currentSerialPort != null && currentSerialPort.IsOpen && !IsConnected)
+                            currentSerialPort.Close();
+                    }
+                    // If its connected break out of the loop
+                    if (IsConnected)
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Disconnect from Arduino Lenardo Serilal Port if its connected
+        /// </summary>
+        public void Disconnect()
+        {
+            // Check for is thre are a connection an close it
+            if (currentSerialPort != null && currentSerialPort.IsOpen && IsConnected)
+            {
+                currentSerialPort.Close();
+                currentSerialPort = null;
+                IsConnected = false;
+            }
+        }
+    }
+}
